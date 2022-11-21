5bd148dbc8b5a159728d785443625eda00b7e528
MORRIS <morris24kariuki@gmail.com>
2022-11-21 05:56:09 +0300

    changed JSsource path in html

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById("hover").addEventListener("mouseover", mouseOver);
 document.getElementById("hover").addEventListener("mouseout", mouseOut);
 
 function mouseOver() {
-    document.getElementById("hover").style.color = "red";
+    document.getElementById("hover").style.color = "orange";
 };
 
 function mouseOut() {
